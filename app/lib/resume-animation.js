8f4a3f0546b4eecc930975b5eaeec03d143eb7d5
Phantr4x <phantr4x@gmail.com>
2016-02-07 11:32:39 +0800

    Achieves Page Demo finished v0.2.5

--- app/lib/resume-animation.js
+++ app/lib/resume-animation.js
@@ -3,8 +3,11 @@ import $ from 'jquery';
 export var ResumeAnimation = {
 	init: function () {
 		$('.mask').hide();
-		$('section .avatar').css('animation', 'drop-in 1.2s cubic-bezier(0.47, 0, 0.75, 0.72) 1.2s 1 alternate forwards');
+		$('section[data-name=home] .avatar').css('animation', 'drop-in 1.2s cubic-bezier(0.47, 0, 0.75, 0.72) 1.2s 1 alternate forwards');
 		$('section[data-name=home] h1, section[data-name=home] p').css('animation', 'rip-in 1.2s cubic-bezier(0.18, 0.89, 0.32, 1.28) 1.2s 1 alternate forwards');
+		$("a[href='#about']").click(function(event) {
+			$('section[data-name=about] .console').css('animation', 'console-in 1.2s ease 120ms 1 alternate forwards');
+		});
 	}
 };
 
